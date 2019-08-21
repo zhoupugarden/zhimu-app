2d944d0f8c21683b2f82c4b6a5df80e8f9b4b494
yu.yang001 <yu.yang001@bkjk.com>
2019-08-21 15:02:46 +0800

    新增vuex&本地存储

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="cart-container">
+    <div class="cart-container__detail">
+      <cart-card
+        :thumbUrl="good.url"
+        :tag="good.tag"
+        :price="good.price"
+        :originPrice="good.originPrice"
+        :title="good.title"
+        :attribute="good.attribute"
+      >
+      </cart-card>
+    </div>
 
-    <cart-card
-      :thumbUrl="good.url"
-      :tag="good.tag"
-      :price="good.price"
-      :originPrice="good.originPrice"
-      :title="good.title"
-      :attribute="good.attribute"
-    >
-    </cart-card>
+    <div v-if="showTip" class="cart-container__tip">
+      请在下方选择需要的配件
+    </div>
 
+    <div class="cart-container__fitting">
+
+    </div>
+
+    <div class="cart-container__bottom">
+      <div class="cart-container__bottom--line">
+      </div>
+      <div class="cart-container__bottom--total">
+        总计：{{totalAmount}}
+        <div class="cart-container__bottom--fee">
+          另需配送费5元
+        </div>
+      </div>
+
+      <div class="cart-container__bottom--button">
+        <van-button size="large" type="primary">下一步</van-button>
+      </div>
+    </div>
 
   </div>
 
@@ -19,6 +42,9 @@
 <script>
 
   import cartCard from '@/components/cartCard';
+
+  import { mapState, mapMutations } from 'vuex';
+  import { SET_OPEN_ID } from '@/store/mutation-types';
 
 
 export default {
@@ -37,10 +63,26 @@ export default {
         originPrice:"100",
         title:"杨宇测试",
         attribute:"1个"
-      }
+      },
+      totalAmount: 1000,
+      showTip:true
     }
 
   },
+  computed: {
+    ...mapState([
+      'openId'
+    ])
+  },
+  methods: {
+    ...mapMutations({
+      setOpenId: 'SET_OPEN_ID'
+    }),
+    storeButton() {
+      this.setOpenId("123456")
+    }
+
+  }
 
 
 
@@ -51,5 +93,36 @@ export default {
   .cart-close {
     background-color: red;
   }
+
+  .cart-container__bottom {
+    width: 100%;
+    height: 100px;
+    position: fixed;
+    bottom: 0;
+    z-index: 100;
+  }
+  .cart-container__bottom--button {
+    position: fixed;
+    bottom: 20px;
+    width: 90%;
+    /*还需要仔细研究如何居中显示*/
+    left: 4%;
+  }
+  .cart-container__tip {
+    padding-top: 20px;
+    font-size: 50%;
+    font-weight: lighter;
+    text-align: center;
+  }
+  .cart-container__bottom--line {
+    width: 100%;
+    height: 1px;
+    border-top: solid #ACC0D8 1px;
+  }
+  .cart-container__bottom--fee {
+    font-size: 50%;
+    display: inline-block;
+  }
+
 </style>
 
